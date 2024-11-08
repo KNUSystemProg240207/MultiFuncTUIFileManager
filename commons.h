ce1ae2714520d31deec05657a64b2f112c573d91
Yunseong Ha <gci28267@gmail.com>
2024-11-08 19:42:10 +0900

    Format Code

--- commons.h
+++ commons.h
@@ -7,55 +7,45 @@
 #include <stdlib.h>
 #include <time.h>
 
+
 // '오류 확인 후 종료' 관련 매크로 함수들
 
-#define CHECK_CURSES1(ret, msg, errno) \
-	do                                 \
-	{                                  \
-		if ((ret) == ERR)              \
-		{                              \
-			fprintf(stderr, (msg));    \
-			exit(errno);               \
-		}                              \
-	} while (0)
+#define CHECK_CURSES1(ret, msg, errno) do {\
+    if ((ret) == ERR) {\
+        fprintf(stderr, (msg));\
+        exit(errno);\
+    }\
+} while (0)
 #define CHECK_CURSES(ret) CHECK_CURSES1(ret, "Error", -1)
 
-#define CHECK_NULL1(ret, msg, errno) \
-	do                               \
-	{                                \
-		if ((ret) == NULL)           \
-		{                            \
-			fprintf(stderr, (msg));  \
-			exit(errno);             \
-		}                            \
-	} while (0)
+#define CHECK_NULL1(ret, msg, errno) do {\
+    if ((ret) == NULL) {\
+        fprintf(stderr, (msg));\
+        exit(errno);\
+    }\
+} while (0)
 #define CHECK_NULL(ret) CHECK_NULL1(ret, "Error", -1)
 
-#define CHECK_FALSE1(ret, msg, errno) \
-	do                                \
-	{                                 \
-		if ((ret) == FALSE)           \
-		{                             \
-			fprintf(stderr, (msg));   \
-			exit(errno);              \
-		}                             \
-	} while (0)
+#define CHECK_FALSE1(ret, msg, errno) do {\
+    if ((ret) == FALSE) {\
+        fprintf(stderr, (msg));\
+        exit(errno);\
+    }\
+} while (0)
 #define CHECK_FALSE(ret) CHECK_FALSE1(ret, "Error", -1)
 
-#define CHECK_FAIL1(ret, msg, errno) \
-	do                               \
-	{                                \
-		if ((ret) != 0)              \
-		{                            \
-			perror((msg));           \
-			exit(errno);             \
-		}                            \
-	} while (0)
+#define CHECK_FAIL1(ret, msg, errno) do {\
+    if ((ret) != 0) {\
+        perror((msg));\
+        exit(errno);\
+    }\
+} while (0)
 #define CHECK_FAIL(ret) CHECK_FAIL1(ret, "Error", -1)
+
 
 /**
  * (현재 시간) - (시작 시간) 계산해서 돌려줌
- *
+ * 
  * @param baseTime 시작 시간 (Clock: CLOCK_MONOTONIC 기준)
  * @return 흐른 시간 (단위: μs)
  */
--- main.c
+++ main.c
@@ -15,6 +15,7 @@
 #include "list_dir.h"
 #include "title_bar.h"
 
+
 WINDOW *titleBar, *bottomBox;
 
 // 각 (폴더의 내용 가져오는) Directory Listener Thread별 저장 공간:
@@ -25,177 +26,163 @@ static char entryNames[MAX_DIRWINS][MAX_STAT_ENTRIES][MAX_NAME_LEN + 1];
 static pthread_cond_t condStopTrd[MAX_DIRWINS];
 static bool stopRequested[MAX_DIRWINS];
 static pthread_mutex_t stopTrdMutex[MAX_DIRWINS];
-static size_t totalReadItems[MAX_DIRWINS] = {0};
+static size_t totalReadItems[MAX_DIRWINS] = { 0 };
 
-static unsigned int dirWinCnt; // 표시된 폴더 표시 창 수
+static unsigned int dirWinCnt;  // 표시된 폴더 표시 창 수
 
-static void initVariables(void); // 변수들 초기화
-static void initScreen(void);	 // ncurses 관련 초기화 & subwindow들 생성
-static void initThreads(void);	 // thread 관련 초기화
-static void mainLoop(void);		 // 프로그램 Main Loop
-static void cleanup(void);		 // atexit()에 전달할 함수: 프로그램 종료 직전, main() 반환 직후에 수행됨
+static void initVariables(void);  // 변수들 초기화
+static void initScreen(void);  // ncurses 관련 초기화 & subwindow들 생성
+static void initThreads(void);  // thread 관련 초기화
+static void mainLoop(void);  // 프로그램 Main Loop
+static void cleanup(void);  // atexit()에 전달할 함수: 프로그램 종료 직전, main() 반환 직후에 수행됨
 
-JobType currentJob = JOB_NONE; // 변수 정의
+JobType currentJob = JOB_NONE;  // 변수 정의
 pthread_mutex_t jobMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t jobCond = PTHREAD_COND_INITIALIZER;
 
-int main(int argc, char **argv)
-{
-	atexit(cleanup);
+int main(int argc, char **argv) {
+    atexit(cleanup);
 
-	initVariables();
-	initScreen();
-	initThreads();
-	mainLoop();
+    initVariables();
+    initScreen();
+    initThreads();
+    mainLoop();
 
-	// Thread들 정지 요청
-	for (int i = 0; i < dirWinCnt; i++)
-	{
-		pthread_mutex_lock(&stopTrdMutex[i]);
-		stopRequested[i] = true;
-		pthread_cond_signal(&condStopTrd[i]);
-		pthread_mutex_unlock(&stopTrdMutex[i]);
-	}
-	// 각 Thread들 대기
-	for (int i = 0; i < dirWinCnt; i++)
-	{
-		pthread_join(threadListDir[i], NULL);
-	}
+    // Thread들 정지 요청
+    for (int i = 0; i < dirWinCnt; i++) {
+        pthread_mutex_lock(&stopTrdMutex[i]);
+        stopRequested[i] = true;
+        pthread_cond_signal(&condStopTrd[i]);
+        pthread_mutex_unlock(&stopTrdMutex[i]);
+    }
+    // 각 Thread들 대기
+    for (int i = 0; i < dirWinCnt; i++) {
+        pthread_join(threadListDir[i], NULL);
+    }
 
-	// 창 '지움' (자원 해제)
-	CHECK_CURSES(delwin(titleBar));
-	CHECK_CURSES(delwin(bottomBox));
+    // 창 '지움' (자원 해제)
+    CHECK_CURSES(delwin(titleBar));
+    CHECK_CURSES(delwin(bottomBox));
 
-	return 0;
+    return 0;
 }
 
-void initVariables(void)
-{
-	// 변수들 기본값으로 초기화
-	for (int i = 0; i < MAX_DIRWINS; i++)
-	{
-		pthread_mutex_init(statMutex + i, NULL);
-		pthread_cond_init(condStopTrd + i, NULL);
-		pthread_mutex_init(stopTrdMutex + i, NULL);
-	}
+void initVariables(void) {
+    // 변수들 기본값으로 초기화
+    for (int i = 0; i < MAX_DIRWINS; i++) {
+        pthread_mutex_init(statMutex + i, NULL);
+        pthread_cond_init(condStopTrd + i, NULL);
+        pthread_mutex_init(stopTrdMutex + i, NULL);
+    }
 }
 
-void initScreen(void)
-{
-	// ncurses 초기화
-	if (initscr() == NULL)
-	{
-		fprintf(stderr, "Window initialization FAILED\n");
-		exit(1);
-	}
-	CHECK_CURSES(cbreak()); // stdin의 line buffering 해제: 키 입력 즉시 받음 (~ICANON과 유사)
-	CHECK_CURSES(noecho()); // echo 끄기: 키 입력 안 보임
-	CHECK_CURSES(nodelay(stdscr, TRUE));
-	CHECK_CURSES(keypad(stdscr, TRUE)); // 특수 키를 일반 키처럼 입력받을 수 있게 함
-	CHECK_CURSES(curs_set(0));			// 커서 숨김
+void initScreen(void) {
+    // ncurses 초기화
+    if (initscr() == NULL) {
+        fprintf(stderr, "Window initialization FAILED\n");
+        exit(1);
+    }
+    CHECK_CURSES(cbreak());  // stdin의 line buffering 해제: 키 입력 즉시 받음 (~ICANON과 유사)
+    CHECK_CURSES(noecho());  // echo 끄기: 키 입력 안 보임
+    CHECK_CURSES(nodelay(stdscr, TRUE));
+    CHECK_CURSES(keypad(stdscr, TRUE));  // 특수 키를 일반 키처럼 입력받을 수 있게 함
+    CHECK_CURSES(curs_set(0));  // 커서 숨김
 
-	CHECK_FALSE1(has_colors(), "Doesn't support color\n", 1); // Color 지원 검사
-	CHECK_CURSES(start_color());							  // Color 시작
-	if (can_change_color() == TRUE)
-	{
-		CHECK_CURSES(init_color(COLOR_WHITE, 1000, 1000, 1000)); // 흰색을 '진짜' 흰색으로: 일부 환경에서, COLOR_WHITE가 회색인 경우 있음
-	}
+    CHECK_FALSE1(has_colors(), "Doesn't support color\n", 1);  // Color 지원 검사
+    CHECK_CURSES(start_color());  // Color 시작
+    if (can_change_color() == TRUE) {
+        CHECK_CURSES(init_color(COLOR_WHITE, 1000, 1000, 1000));  // 흰색을 '진짜' 흰색으로: 일부 환경에서, COLOR_WHITE가 회색인 경우 있음
+    }
 
-	// 창 크기 가져옴
-	int h, w;
-	getmaxyx(stdscr, h, w);
+    // 창 크기 가져옴
+    int h, w;
+    getmaxyx(stdscr, h, w);
 
-	titleBar = initTitleBar(w);					   // 제목 창 (프로그램 이름 - 현재 경로 - 현재 시간) 생성
-	bottomBox = initBottomBox(w, h - 2);		   // 아래쪽 단축키 창 생성
-	CHECK_CURSES(mvhline(1, 0, ACS_HLINE, w));	   // 제목 창 아래로 가로줄 그림
-	CHECK_CURSES(mvhline(h - 3, 0, ACS_HLINE, w)); // 단축키 창 위로 가로줄 그림
+    titleBar = initTitleBar(w);  // 제목 창 (프로그램 이름 - 현재 경로 - 현재 시간) 생성
+    bottomBox = initBottomBox(w, h - 2);  // 아래쪽 단축키 창 생성
+    CHECK_CURSES(mvhline(1, 0, ACS_HLINE, w));  // 제목 창 아래로 가로줄 그림
+    CHECK_CURSES(mvhline(h - 3, 0, ACS_HLINE, w));  // 단축키 창 위로 가로줄 그림
 
-	initDirWin(&statMutex[0], statEntries[0], entryNames[0], &totalReadItems[0]); // 폴더 내용 표시 창 생성
-	dirWinCnt = 1;
+    initDirWin(&statMutex[0], statEntries[0], entryNames[0], &totalReadItems[0]);  // 폴더 내용 표시 창 생성
+    dirWinCnt = 1;
 }
 
-void initThreads(void)
-{
-	startDirListender(
-		&threadListDir[0], &statMutex[0],
-		statEntries[0], entryNames[0], MAX_STAT_ENTRIES,
-		&totalReadItems[0], &condStopTrd[0], &stopRequested[0], &stopTrdMutex[0]); // Directory Listener Thread 시작
+void initThreads(void) {
+    startDirListender(
+        &threadListDir[0], &statMutex[0],
+        statEntries[0], entryNames[0], MAX_STAT_ENTRIES,
+        &totalReadItems[0], &condStopTrd[0], &stopRequested[0], &stopTrdMutex[0]
+    );  // Directory Listener Thread 시작
 }
 
-void mainLoop(void)
-{
-	struct timespec startTime;
-	uint64_t elapsedUSec;
-	char cwdBuf[MAX_CWD_LEN];
+void mainLoop(void) {
+    struct timespec startTime;
+    uint64_t elapsedUSec;
+    char cwdBuf[MAX_CWD_LEN];
 
-	char *cwd;
-	while (1)
-	{
-		clock_gettime(CLOCK_MONOTONIC, &startTime); // 시작 시간
+    char *cwd;
+    while (1) {
+        clock_gettime(CLOCK_MONOTONIC, &startTime);  // 시작 시간
 
-		// 키 입력 처리
-		for (int ch = wgetch(stdscr); ch != ERR; ch = wgetch(stdscr))
-		{
-			switch (ch)
-			{
-			case '\n':
-				currentJob = JOB_ENTER;
-				break;
-			case 'c': // Copy
-				currentJob = JOB_COPY;
-				break;
-			case 'm': // Move
-				currentJob = JOB_MOVE;
-				break;
-			case 'r': // Remove
-				currentJob = JOB_REMOVE;
-				break;
-			case KEY_UP:
-				moveCursorUp();
-				break;
-			case KEY_DOWN:
-				moveCursorDown();
-				break;
-			case KEY_LEFT:
-				selectNextWindow();
-				break;
-			case KEY_RIGHT:
-				selectPreviousWindow();
-				break;
-			case 'q':
-			case 'Q':
-				goto CLEANUP; // Main Loop 빠져나감
-			}
-		}
+        // 키 입력 처리
+        for (int ch = wgetch(stdscr); ch != ERR; ch = wgetch(stdscr)) {
+            switch (ch) {
+                case '\n':
+                    currentJob = JOB_ENTER;
+                    break;
+                case 'c':  // Copy
+                    currentJob = JOB_COPY;
+                    break;
+                case 'm':  // Move
+                    currentJob = JOB_MOVE;
+                    break;
+                case 'r':  // Remove
+                    currentJob = JOB_REMOVE;
+                    break;
+                case KEY_UP:
+                    moveCursorUp();
+                    break;
+                case KEY_DOWN:
+                    moveCursorDown();
+                    break;
+                case KEY_LEFT:
+                    selectNextWindow();
+                    break;
+                case KEY_RIGHT:
+                    selectPreviousWindow();
+                    break;
+                case 'q':
+                case 'Q':
+                    goto CLEANUP;  // Main Loop 빠져나감
+            }
+        }
 
-		// 제목 영역 업데이트
-		renderTime(); // 시간 업데이트
-		// TODO: get current window's cwd
-		char *cwd = getcwd(cwdBuf, MAX_CWD_LEN); // 경로 가져옴
-		if (cwd == NULL)
-			printPath("-----"); // 경로 가져오기 실패 시
-		else
-			printPath(cwd); // 경로 업데이트
+        // 제목 영역 업데이트
+        renderTime();  // 시간 업데이트
+        // TODO: get current window's cwd
+        char *cwd = getcwd(cwdBuf, MAX_CWD_LEN);  // 경로 가져옴
+        if (cwd == NULL)
+            printPath("-----");  // 경로 가져오기 실패 시
+        else
+            printPath(cwd);  // 경로 업데이트
 
-		updateDirWins(); // 폴더 표시 창들 업데이트
+        updateDirWins();  // 폴더 표시 창들 업데이트
 
-		// 필요한 창들 refresh
-		CHECK_CURSES(wrefresh(titleBar));
-		CHECK_CURSES(wrefresh(bottomBox));
-		refresh();
+        // 필요한 창들 refresh
+        CHECK_CURSES(wrefresh(titleBar));
+        CHECK_CURSES(wrefresh(bottomBox));
+        refresh();
 
-		// Frame rate 제한: delay - 경과 time
-		elapsedUSec = getElapsedTime(startTime);
-		if (FRAME_INTERVAL_USEC > elapsedUSec)
-		{
-			usleep(FRAME_INTERVAL_USEC - elapsedUSec); // Main thread 일시정지
-		}
-	}
+        // Frame rate 제한: delay - 경과 time
+        elapsedUSec = getElapsedTime(startTime);
+        if (FRAME_INTERVAL_USEC > elapsedUSec) {
+            usleep(FRAME_INTERVAL_USEC - elapsedUSec);  // Main thread 일시정지
+        }
+    }
 CLEANUP:
-	return;
+    return;
 }
 
-void cleanup(void)
-{
-	endwin();
+void cleanup(void) {
+    endwin();
 }
