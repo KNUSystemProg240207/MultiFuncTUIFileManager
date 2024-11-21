c9de847d49d0619d72d64625624097fb64087c15
mdy99 <0909minty@gmail.com>
2024-11-21 22:47:42 +0900

    함수 이름 자바형식으로 바꾸기, 색깔 덮어쓰기는 실패

--- colors.h
+++ colors.h
@@ -15,14 +15,16 @@ typedef enum {
     DEFAULT
 } FILE_COLOR;
 
-#define COLOR_ORANGE 1
-#define COLOR_NAVY 2
-#define COLOR_DARKGRAY 3
-#define COLOR_LESS_WHITE 4
-#define COLOR_HOT_PINK 5
-#define COLOR_GREEN_BLUE 6
-#define COLOR_ALL_BLUE 8
+#define COLOR_ORANGE 1  // 주황색
+#define COLOR_NAVY 2  // 네이비 색상
+#define COLOR_DARKGRAY 3  // 어두운 회색
+#define COLOR_LESS_WHITE 4  // 덜 밝은 흰색
+#define COLOR_HOT_PINK 5  // 핫핑크
+#define COLOR_GREEN_BLUE 6  // 초록빛 파랑색
+// COLOR ID 7은 의도적으로 건너뛴 상태
+#define COLOR_ALL_BLUE 8  // 전체 파란색
 
+void calc255Color(short color_name, short r, short g, short b);
 void initColorSet();
 
 
